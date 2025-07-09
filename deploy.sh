1dac19884abfaa2b4d5600ba31f4b1c868625951
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2025-07-09 12:39:30 -0700

    chore: fixing cronjob

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,8 @@
 ### and deploy the helm chart with the required values.
 set -euo pipefail
 
-git_sha1=$(git rev-parse HEAD)
-
-dagConfig=$(echo "{\"org\": \"bcgov\", \"repo\": \"cas-ciip-portal\", \"ref\": \"$(git_sha1)\", \"path\": \"dags/cas_data_warehouse_import_data_sources.py\"}" | base64 -w0); 
+gitsha1=$(git rev-parse HEAD)
+dagConfig=$(echo "{\"org\": \"bcgov\", \"repo\": \"cas-data-warehouse\", \"ref\": \"$gitsha1\", \"path\": \"dags/cas_data_warehouse_import_data_sources.py\"}" | base64 -w0); 
 
 helm dep up ./helm/cas-data-warehouse
 helm repo add cas-postgres https://bcgov.github.io/cas-postgres/
@@ -16,5 +15,5 @@ helm upgrade --install --timeout 900s \
   --namespace "$CIIP_NAMESPACE_PREFIX-$ENVIRONMENT" \
   -f ./helm/cas-data-warehouse/values.yaml \
   -f "./helm/cas-data-warehouse/values-$ENVIRONMENT.yaml" \
-  --set download-cas-ciip-portal-dags.dagConfiguration="$dagConfig" \
+  --set download-cas-data-warehouse-dags.dagConfiguration="$dagConfig" \
   cas-data-warehouse ./helm/cas-data-warehouse 
